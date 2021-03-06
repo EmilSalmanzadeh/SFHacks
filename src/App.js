a691c7c875b5c40f23434f90662768f9467162ee
EmilSalmanzadeh <esalmanz@ucsd.edu>
2021-03-06 00:33:46 -0800

    first night

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import Button from '@material-ui/core/Button';
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World Market</h1><br/>
+      <h2> Yo</h2> 
+      <h6>Kristin and Emil</h6>
+      <p>Testing123</p>
       <h2>Start editing to see some magic happen!</h2>
+      <Button variant="outlined">Default</Button>
+      <Button variant="outlined" color="primary">
+        Primary
+      </Button>
+      <Button variant="outlined" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="outlined" disabled>
+        Disabled
+      </Button>
+      <Button variant="outlined" color="primary" href="#outlined-buttons">
+        Link
+      </Button>
     </div>
   );
 }
